141aee782af826d0f67534a404dae7807499e041
StaubL <Lutscher45@>
2024-12-12 12:45:31 +0100

    Updated dashboard design with background image and logout functionality

--- src/views/Dashboard/Dashboard.ts
+++ src/views/Dashboard/Dashboard.ts
@@ -3,7 +3,8 @@ import template from "./Dashboard.html";
 
 @customElement({ name: "Dashboard", template })
 export class Dashboard {
-  myMethod() {
-    alert("You are now on the Dashboard page!");
+  logout() {
+    alert("You have been logged out.");
+    window.location.href = "/";
   }
 }
